a79edfaabd94a88ef2d2a71e1d906eb4b91120b6
Pitmon Foxall <foxthe4th@gmail.com>
2022-10-21 00:09:26 -0600

    Made changes to account for input list a and b being different sizes.

--- src/main/Java/IntersectingPoint.java
+++ src/main/Java/IntersectingPoint.java
@@ -6,8 +6,14 @@ public class IntersectingPoint {
         int evaluateA;
         int evaluateB;
         int result = 0;
+        int shorterList;
+        if(listA.size() < listB.size()){
+            shorterList = listA.size();
+        } else {
+            shorterList = listB.size();
+        }
 
-        for(int i = 0; i < listA.size(); i++){
+        for(int i = 0; i < shorterList; i++){
             evaluateA = (int) listA.get(i);
             evaluateB = (int) listB.get(i);
 
@@ -15,7 +21,6 @@ public class IntersectingPoint {
                 result = listA.indexOf(evaluateA);
             }
         }
-
         return result;
     }
 
@@ -27,15 +32,18 @@ public class IntersectingPoint {
         listA.add(1, 55);
         listA.add(2, 43);
         listA.add(3, 67);
-        listA.add(4, 102);
-        listA.add(5, 27);
+        listA.add(4, 74);
+        listA.add(5, 270);
+        listA.add(6, 91);
+        listA.add(7, 34);
+        listA.add(8, 2);
 
         listB.add(0, 38);
         listB.add(1, 81);
         listB.add(2, 33);
-        listB.add(3, 67);
+        listB.add(3, 66);
         listB.add(4, 12);
-        listB.add(5, 105);
+        listB.add(5, 270);
 
         int result = findIntersect(listA,listB);
         System.out.println(result);
